eb3d5387fd91f4bf61a9d0a49566fe0f5b246902
Tomislav <44715559+TVCTC@users.noreply.github.com>
2022-10-15 21:43:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "This is your stream: " . $_GET["stream"];
+$url = $_GET['get_headers_from_stream'];
+$options = array('http' =>
+    array(
+        'method' => 'GET',
+        'user_agent' => 'The Best User Agent Ever',
+        'header' => 'Icy-MetaData: 1'
+        )
+    );
+$context = stream_context_create($options);
+$headers = get_headers($url, false, $context);
+$json = json_encode($headers);
+echo $json;
 ?>
